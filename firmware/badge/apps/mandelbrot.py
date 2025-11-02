85e4848391d8b726a214b9f279ce3d80eab1608a
Chandler <jcjearls@gmail.com>
2025-11-01 22:43:11 -0700

    changing so we don't override with the same buffer over and over again and we're instead setting pixels

--- firmware/badge/apps/mandelbrot.py
+++ firmware/badge/apps/mandelbrot.py
@@ -141,15 +141,8 @@ class App(BaseApp):
 
                 # Then convert it to the display's RGB565 format
                 color_24bit = cycle_colors(iterations, self.mandelbrot_iterations+1)
-                color =  generate_565_color((color_24bit>>16)&0xFF, (color_24bit>>8)&0xFF, color_24bit&0xFF)
-                
-                #Then get the upper and lower bytes of the color for writing to the buffer
-                upper_color_byte = color >> 8
-                lower_color_byte = color & 0xFF
-                self.canvas_buffer[2*x+self.x_width*2*y] = lower_color_byte
-                self.canvas_buffer[2*x+1+self.x_width*2*y] = upper_color_byte
-            # By setting the buffer, we tell the display to update with the new data we've written to it
-            self.canvas.set_buffer(self.canvas_buffer,self.x_width,self.y_height,lvgl.COLOR_FORMAT.RGB565)
+
+                self.canvas.set_px(x, y, lvgl.color_hex(color_24bit), True)
 
         # Get the deltas of each pixel with its neighbors and put them in an array
         delta_array = get_iteration_deltas(self.iteration_array)
